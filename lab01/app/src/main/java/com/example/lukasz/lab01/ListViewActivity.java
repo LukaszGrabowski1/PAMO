4b08d735a654bce61c9effbaa44f5801d77f8b08
Łukasz Grabowski <lukasz.grabowski6@gmail.com>
2017-09-27 18:46:44 +0200

    lab01 zad07

--- lab01/app/src/main/java/com/example/lukasz/lab01/ListViewActivity.java
+++ lab01/app/src/main/java/com/example/lukasz/lab01/ListViewActivity.java
@@ -1,5 +1,6 @@
 package com.example.lukasz.lab01;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,21 +18,22 @@ public class ListViewActivity extends AppCompatActivity {
 
         final ListView listView = (ListView) findViewById(R.id.list_view);
 
-        final String[] pages = new String[2];
+        final String[] pages = new String[3];
         pages[0] = "http://www.gdansk.pjwstk.edu.pl";
         pages[1] = "http://www.pjwstk.edu.pl";
+        pages[2] = "http://www.google.pl";
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, pages);
 
         listView.setAdapter(adapter);
 
+        final Intent webViewIntent = new Intent(this, WebViewActivity.class);
+
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                CharSequence text = adapterView.getItemAtPosition(i).toString();
-                int duration = Toast.LENGTH_SHORT;
-                Toast toast = Toast.makeText(getApplicationContext(), text, duration);
-                toast.show();
+                webViewIntent.putExtra("url", pages[i]);
+                startActivity(webViewIntent);
             }
         });
     }
