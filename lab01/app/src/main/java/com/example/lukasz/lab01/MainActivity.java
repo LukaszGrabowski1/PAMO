ce74b4bdc5699978499d64cbabef1d6cb823e49c
Łukasz Grabowski <lukasz.grabowski6@gmail.com>
2017-09-27 16:16:13 +0200

    lab01 zad02

--- lab01/app/src/main/java/com/example/lukasz/lab01/MainActivity.java
+++ lab01/app/src/main/java/com/example/lukasz/lab01/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.lukasz.lab01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView textView = (TextView) findViewById(R.id.textView);
+        final Button buttonChangeText = (Button) findViewById(R.id.changeTextButton);
+
+
+        buttonChangeText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = textView.getText().toString();
+                textView.setText("HELLO " + text);
+            }
+        });
     }
 }
